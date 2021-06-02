d23d1ce6a615014e07af0afbe18216228c49335a
AL-SAFWAN <al-safwan.uddin@abehr.com>
2021-06-02 17:42:25 +0100

    header compnent section completed

--- components/Header.js
+++ components/Header.js
@@ -4,6 +4,7 @@ import {
   FlagIcon,
   PlayIcon,
   ShoppingCartIcon,
+  ChevronLeftIcon,
 } from "@heroicons/react/outline";
 import {
   BellIcon,
@@ -28,7 +29,7 @@ function Header() {
         <div className="flex ml-2 items-center rounded-full bg-gray-100 p-2">
           <SearchIcon className="h-6 text-gray-600" />
           <input
-            className="flex ml-2 items-center bg-transparent outline-none placeholder-gray-500 flex-shrink"
+            className="hidden md:inline-flex  flex ml-2 items-center bg-transparent outline-none placeholder-gray-500 flex-shrink"
             type="text"
             placeholder="Search Facebook"
           />
@@ -42,6 +43,13 @@ function Header() {
           <HeaderIcon Icon={ShoppingCartIcon} />
           <HeaderIcon Icon={UserGroupIcon} />
         </div>
+      </div>
+      <div className="flex items-center sm:space-x-2 justify-end">
+        <p className=" whitespace-nowrap font-semibold pr-3">Safwan</p>
+        <ViewGridIcon className="icon" />
+        <ChatIcon className="icon" />
+        <BellIcon className="icon" />
+        <ChevronLeftIcon className="icon" />
       </div>
     </div>
   );
