3a527f4dadbce4f6beed46739e6cba19c0cd404e
AL-SAFWAN <al-safwan.uddin@abehr.com>
2021-06-02 17:36:27 +0100

    header center section

--- components/Header.js
+++ components/Header.js
@@ -13,10 +13,11 @@ import {
   UserGroupIcon,
   ViewGridIcon,
 } from "@heroicons/react/solid";
+import HeaderIcon from "./HeaderIcon";
 
 function Header() {
   return (
-    <div>
+    <div className="sticky top-0 z-50 bg-white flex items-center p-2 lg:px-5 shadow-md">
       <div className="flex items-center">
         <Image
           src="https://links.papareact.com/5me"
@@ -27,10 +28,19 @@ function Header() {
         <div className="flex ml-2 items-center rounded-full bg-gray-100 p-2">
           <SearchIcon className="h-6 text-gray-600" />
           <input
-            className="flex ml-2 items-center bg-transparent outline-none placeholder-gray-500"
+            className="flex ml-2 items-center bg-transparent outline-none placeholder-gray-500 flex-shrink"
             type="text"
             placeholder="Search Facebook"
           />
+        </div>
+      </div>
+      <div className="flex justify-center flex-grow">
+        <div className="flex space-x-6 md:space-x-2">
+          <HeaderIcon Icon={HomeIcon} active />
+          <HeaderIcon Icon={FlagIcon} />
+          <HeaderIcon Icon={PlayIcon} />
+          <HeaderIcon Icon={ShoppingCartIcon} />
+          <HeaderIcon Icon={UserGroupIcon} />
         </div>
       </div>
     </div>
